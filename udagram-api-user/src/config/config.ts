852ae310edb981e8c8d4ce81553cd109fc4cfe3c
crytochap <olusegunsamson@live.com>
2022-07-19 14:25:08 +0100

    fix reverse  proxy

--- udagram-api-user/src/config/config.ts
+++ udagram-api-user/src/config/config.ts
@@ -1,14 +1,14 @@
 export const config = {
-  'username': process.env.POSTGRES_USERNAME,
-  'password': process.env.POSTGRES_PASSWORD,
-  'database': process.env.POSTGRES_DB,
-  'host': process.env.POSTGRES_HOST,
-  'dialect': 'postgres',
-  'aws_region': process.env.AWS_REGION,
-  'aws_profile': process.env.AWS_PROFILE,
-  'aws_media_bucket': process.env.AWS_BUCKET,
-  'url': process.env.URL,
-  'jwt': {
-    'secret': process.env.JWT_SECRET,
+  username: "postgres",
+  password: "y69pvrFje4g3kp8YTgsq",
+  database: "postgres",
+  host: "udagram-micro.c7o5iik2muwy.us-east-1.rds.amazonaws.com",
+  dialect: "postgres",
+  aws_region: "us-east-1",
+  aws_profile: "default",
+  aws_media_bucket: "udagram-micro",
+  url: process.env.URL,
+  jwt: {
+    secret: "testing",
   },
 };
